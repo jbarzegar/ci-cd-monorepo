7c9b0feffd274b2f84df2262e399fddb0f6d9ceb
James Barzegar <james.barzegar@gmail.com>
2020-03-25 14:28:03 -0400

    feat(some-app): add some text

--- apps/some-app/src/App.js
+++ apps/some-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <span>Boom edited</span>
+        <span>Boom edited do something</span>
         <a
           className="App-link"
           href="https://reactjs.org"
