c7e47b86de3fa1cec8c7d216e5e3ff52774e2e58
James Barzegar <james.barzegar@cover.com>
2020-04-16 11:14:13 -0400

    feat: something else

---
+++ scripts/find-package-path.js
@@ -0,0 +1,11 @@
+let { isRequired } = require("./utils");
+let getPackages = require("./get-lerna-packages");
+
+let main = async () => {
+  console.log("ye");
+};
+
+if (isRequired()) {
+} else {
+  main();
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -4,10 +4,13 @@ const ROOT_PATH = path.join(__dirname, "..");
 const NODE_BIN = path.resolve(ROOT_PATH, "node_modules/.bin");
 const lernaCmd = path.join(NODE_BIN, "lerna");
 
+let isRequired = () => require.main === module;
+
 module.exports = {
   paths: {
     ROOT_PATH,
     NODE_BIN,
     lernaCmd
-  }
+  },
+  isRequired
 };
