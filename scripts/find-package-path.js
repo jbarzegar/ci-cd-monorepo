b24061dd4b629bc939b19c3fcf37bb09b4890905
James Barzegar <james.barzegar@gmail.com>
2020-04-16 11:27:04 -0400

    feat: something else
    *
    
    * feat: something else else
    
    Co-authored-by: James Barzegar <james.barzegar@cover.com>

---
+++ scripts/find-package-path.js
@@ -0,0 +1,13 @@
+let { isRequired } = require("./utils");
+let getPackages = require("./get-lerna-packages");
+
+let main = async () => {
+  console.log("ye");
+};
+
+console.log("something");
+
+if (isRequired()) {
+} else {
+  main();
+}
